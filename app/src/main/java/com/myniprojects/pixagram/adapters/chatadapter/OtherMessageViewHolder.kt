44645b7fb237bcb828289accfc90cdfac00a31ea
Rydzyk Cezary <rydzykcezary@gmail.com>
2021-05-02 14:03:14 +0200

    Styling messages

--- app/src/main/java/com/myniprojects/pixagram/adapters/chatadapter/OtherMessageViewHolder.kt
+++ app/src/main/java/com/myniprojects/pixagram/adapters/chatadapter/OtherMessageViewHolder.kt
@@ -1,10 +1,14 @@
 package com.myniprojects.pixagram.adapters.chatadapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
+import com.google.android.material.shape.CornerFamily
+import com.myniprojects.pixagram.R
 import com.myniprojects.pixagram.databinding.MessageOtherItemBinding
+import com.myniprojects.pixagram.utils.ext.context
 
 class OtherMessageViewHolder private constructor(
     private val binding: MessageOtherItemBinding
@@ -28,10 +32,58 @@ class OtherMessageViewHolder private constructor(
         glide: RequestManager
     )
     {
-        binding.txtBody.text = message.chatMessage.textContent
+        with(binding)
+        {
+            txtBody.text = message.chatMessage.textContent
 
-        glide
-            .load(message.user.imageUrl)
-            .into(binding.imgAvatar)
+            glide
+                .load(message.user.imageUrl)
+                .into(imgAvatar)
+
+
+            // region card styling
+
+            val r = binding.context.resources.getDimension(R.dimen.message_corner_radius)
+
+            val b = cardView.shapeAppearanceModel.toBuilder()
+                .setAllCorners(CornerFamily.ROUNDED, r)
+
+            when (message.type)
+            {
+                MessageType.FIRST ->
+                {
+                    b.setTopLeftCornerSize(0f)
+                    b.setBottomLeftCornerSize(0f)
+                }
+                MessageType.MIDDLE ->
+                {
+                    b.setTopLeftCornerSize(0f)
+                    b.setBottomLeftCornerSize(0f)
+                }
+                MessageType.LAST ->
+                {
+                    b.setBottomLeftCornerSize(0f)
+                }
+                MessageType.SINGLE ->
+                {
+                    b.setBottomLeftCornerSize(0f)
+                }
+            }
+
+            cardView.shapeAppearanceModel = b.build()
+
+            // endregion
+
+            if (message.type == MessageType.FIRST || message.type == MessageType.SINGLE)
+            {
+                imgAvatar.visibility = View.VISIBLE
+            }
+            else
+            {
+                imgAvatar.visibility = View.INVISIBLE
+            }
+
+
+        }
     }
 }
--- app/src/main/java/com/myniprojects/pixagram/adapters/chatadapter/OwnMessageViewHolder.kt
+++ app/src/main/java/com/myniprojects/pixagram/adapters/chatadapter/OwnMessageViewHolder.kt
@@ -3,7 +3,10 @@ package com.myniprojects.pixagram.adapters.chatadapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.shape.CornerFamily
+import com.myniprojects.pixagram.R
 import com.myniprojects.pixagram.databinding.MessageOwnItemBinding
+import com.myniprojects.pixagram.utils.ext.context
 
 class OwnMessageViewHolder private constructor(
     private val binding: MessageOwnItemBinding
@@ -26,6 +29,43 @@ class OwnMessageViewHolder private constructor(
         message: MassageModel.OwnMessage,
     )
     {
-        binding.txtBody.text = message.chatMessage.textContent
+
+        with(binding)
+        {
+            txtBody.text = message.chatMessage.textContent
+
+            // region card styling
+
+            val r = binding.context.resources.getDimension(R.dimen.message_corner_radius)
+
+            val b = cardView.shapeAppearanceModel.toBuilder()
+                .setAllCorners(CornerFamily.ROUNDED, r)
+
+            when (message.type)
+            {
+                MessageType.FIRST ->
+                {
+                    b.setTopRightCornerSize(0f)
+                }
+                MessageType.MIDDLE ->
+                {
+                    b.setTopRightCornerSize(0f)
+                    b.setBottomRightCornerSize(0f)
+                }
+                MessageType.LAST ->
+                {
+                    b.setBottomRightCornerSize(0f)
+                }
+                MessageType.SINGLE ->
+                {
+
+                }
+            }
+
+            cardView.shapeAppearanceModel = b.build()
+
+            // endregion
+        }
+
     }
 }
